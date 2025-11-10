e15a9bfa4eab279276ec4848eb92b62a2c8c4ad1
hello84037 <hello84037@gmail.com>
2025-11-10 14:05:03 -0700

    Add unit tests for SBFLApp

---
+++ SBFLApp.Tests/SpectrumTests.cs
@@ -0,0 +1,40 @@
+using SBFLApp;
+
+namespace SBFLApp.Tests;
+
+public class SpectrumTests
+{
+    [Fact]
+    public void ResetInstrumentation_RemovesCoverageStatements()
+    {
+        var tempDirectory = Directory.CreateTempSubdirectory();
+        try
+        {
+            var filePath = Path.Combine(tempDirectory.FullName, "Sample.cs");
+            var originalContent = """
+using System;
+
+class Sample
+{
+    void Method()
+    {
+        System.IO.File.AppendAllText("test.coverage.tmp", "data");
+        Console.WriteLine("Hello");
+    }
+}
+""";
+            File.WriteAllText(filePath, originalContent);
+
+            Spectrum.ResetInstrumentation(new[] { filePath });
+
+            var updatedContent = File.ReadAllText(filePath);
+
+            Assert.DoesNotContain("AppendAllText", updatedContent, StringComparison.Ordinal);
+            Assert.Contains("Console.WriteLine(\"Hello\");", updatedContent, StringComparison.Ordinal);
+        }
+        finally
+        {
+            tempDirectory.Delete(recursive: true);
+        }
+    }
+}
