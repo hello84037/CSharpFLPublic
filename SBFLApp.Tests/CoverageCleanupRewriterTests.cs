614f746891681ce019d7d02ca8c702e5ac920894
hello84037 <hello84037@gmail.com>
2025-11-10 15:39:16 -0700

    Disable parallel execution in SBFLApp tests

---
+++ SBFLApp.Tests/CoverageCleanupRewriterTests.cs
@@ -0,0 +1,35 @@
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SBFLApp;
+
+namespace SBFLApp.Tests;
+
+public class CoverageCleanupRewriterTests
+{
+    [Fact]
+    public void VisitExpressionStatement_RemovesCoverageStatement()
+    {
+        const string statementText = "System.IO.File.AppendAllText(\"test.coverage.tmp\", \"data\");";
+        var statement = SyntaxFactory.ParseStatement(statementText) as ExpressionStatementSyntax;
+        Assert.NotNull(statement);
+
+        var rewriter = new CoverageCleanupRewriter();
+        var result = rewriter.Visit(statement!);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void VisitExpressionStatement_PreservesNonCoverageStatement()
+    {
+        const string statementText = "Console.WriteLine(\"Hello\");";
+        var statement = SyntaxFactory.ParseStatement(statementText) as ExpressionStatementSyntax;
+        Assert.NotNull(statement);
+
+        var rewriter = new CoverageCleanupRewriter();
+        var result = rewriter.Visit(statement!);
+
+        Assert.NotNull(result);
+        Assert.Equal(statementText, result!.ToString());
+    }
+}
