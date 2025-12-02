94121f226f1f6e58fef78abaf7ff6a28d9279aa7
hello84037 <hello84037@gmail.com>
2025-12-02 12:09:21 -0700

    Exception handling

--- SBFLApp/Rank.cs
+++ SBFLApp/Rank.cs
@@ -250,17 +250,49 @@ namespace SBFLApp
                 Directory.CreateDirectory(directory);
             }
 
-            using var writer = new StreamWriter(filePath, false);
-
-            switch (format)
+            if(IsFileInUse(filePath))
             {
-                case SuspiciousnessReportFormat.Markdown:
-                    WriteMarkdownReport(writer, snapshot);
-                    break;
-                default:
-                    WriteCsvReport(writer, snapshot);
-                    break;
+                System.Console.WriteLine($"The file {filePath} is currently open.  Please close it and then press any key");
+                System.Console.ReadKey();
             }
+
+            try
+            {
+                using var writer = new StreamWriter(filePath, false);
+
+                switch (format)
+                {
+                    case SuspiciousnessReportFormat.Markdown:
+                        WriteMarkdownReport(writer, snapshot);
+                        break;
+                    default:
+                        WriteCsvReport(writer, snapshot);
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                System.Console.WriteLine($"The file {filePath} is still open.  Unable to write the report.");
+            }
+        }
+
+        private static bool IsFileInUse(string filePath)
+        {
+            try
+            {
+                using var writer = new StreamWriter(filePath, false);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+
+            //file is not locked
+            return false;
         }
 
         private static void WriteCsvReport(StreamWriter writer, SuspiciousnessReportSnapshot snapshot)
